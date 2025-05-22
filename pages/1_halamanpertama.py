5f40ab932638eddf23cc62d26afa26980d98a5ce
jaefri1 <jaefriazzamie@students.unnes.ac.id>
2025-05-22 09:15:41 +0700

    Update 1_halamanpertama.py

--- pages/1_halamanpertama.py
+++ pages/1_halamanpertama.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import joblib
 
-st.set_page_config(pahe_title="Prediction",page_icon="1")
 st.title("Halaman Pertama")
 st.write("berikut adalah halaman pertama, Liverpool adalah tim terbaik di dunia!")
